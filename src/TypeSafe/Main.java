6a0ebb943ac4b4fa637f48797b0ae7ccd39ed3ca
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-01 14:53:44 +0900

    printsの出力処理をmainに実装

--- src/TypeSafe/Main.java
+++ src/TypeSafe/Main.java
@@ -3,10 +3,10 @@ package TypeSafe;
 public class Main {
 
 	public static void main(String[] args) {
-
+		prints("仮面ライダー",6);
 	}
 	//第一引数の文字を第二引数の数分表示する
-	public void prints(int a,int b) {
+	public static void prints(String a,int b) {
 		for(int i = 0;i<b;i++) {
 			System.out.println(a);
 		}
