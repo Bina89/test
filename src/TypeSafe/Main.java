bbcd5cf58d326548dfafc21d4b62d699eeeb6ad2
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-01 14:34:52 +0900

    文字の複数回表示メソッド作成（いままで）

---
+++ src/TypeSafe/Main.java
@@ -0,0 +1,14 @@
+package TypeSafe;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+	//第一引数の文字を第二引数の数分表示する
+	public void prints(int a,int b) {
+		for(int i = 0;i<b;i++) {
+			System.out.println(a);
+		}
+	}
+}
