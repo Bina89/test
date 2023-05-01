3e125344e99efe6e7bf8c6a188bc251458d3fee2
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-01 14:56:16 +0900

    printsメソッドの引数StringをObjectに変更

--- src/TypeSafe/Main.java
+++ src/TypeSafe/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		prints("仮面ライダー",6);
 	}
 	//第一引数の文字を第二引数の数分表示する
-	public static void prints(String a,int b) {
+	public static void prints(Object a,int b) {
 		for(int i = 0;i<b;i++) {
 			System.out.println(a);
 		}
