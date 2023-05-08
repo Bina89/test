91757680b4aa0b470fba4e905a7fedb9d2490e66
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-08 14:40:20 +0900

    Accountにgettersetter作成

--- src/Enum/Account.java
+++ src/Enum/Account.java
@@ -4,4 +4,12 @@ public class Account {
 	private String accountNo;	//口座番号
 	private int zandaka;		//残高
 	private String accountType;//預金種類
+
+	public void setaccountNo(String no) {this.accountNo = no;}
+	public void setzandaka(int zan) {this.zandaka = zan;}
+	public void setaccountType(String type) {this.accountType = type;}
+
+	public String getaccountNo() {return this.accountNo;}
+	public int zandaka () {return this.zandaka;}
+	public String accountType() {return this.accountType;}
 }
