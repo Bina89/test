abadd68863c14f681bdc4f74db41c87cd94af4fb
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-08 12:07:09 +0900

    mainでPocketのdataを表示

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -3,8 +3,16 @@ package generics;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Pocket p = new Pocket();
 
+//		データの入力
+		p.put("ローマ");
+
+//		データを取り出し変数に格納 Objectなのでキャスト
+		String s = (String)p.get();
+
+//		表示
+		System.out.println(s);
 	}
 
 }
