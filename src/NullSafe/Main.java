33a5c63718283afd1ed6ad9a0ab4547f130b9b7b
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-09 12:22:49 +0900

    nullが格納された変数を作成

--- src/NullSafe/Main.java
+++ src/NullSafe/Main.java
@@ -1,11 +1,17 @@
 package NullSafe;
 
+import java.util.Optional;
+
 public class Main {
 
 	public static void main(String[] args) {
-		String s = null;
-		
-		s.charAt(0);
+		//nullかどうかチェックしつつインスタンス生成をする
+		//newで生成は不可
+		Optional<String> op1 = Optional.ofNullable("うんち");
+		//nullの格納を許さない
+		//Optional<String> op2 = Optional.of(null);
+
+		Optional<String> op2 = Optional.ofNullable(null);
 
 	}
 
