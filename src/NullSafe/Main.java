abd2609d7d5de0af7f7a7542cff595d1afe6b2d6
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-09 12:28:02 +0900

    作成した変数を表示

--- src/NullSafe/Main.java
+++ src/NullSafe/Main.java
@@ -5,13 +5,20 @@ import java.util.Optional;
 public class Main {
 
 	public static void main(String[] args) {
-		//nullかどうかチェックしつつインスタンス生成をする
+		
+		//nullが入るインスタンス生成をする
 		//newで生成は不可
 		Optional<String> op1 = Optional.ofNullable("うんち");
-		//nullの格納を許さない
-		//Optional<String> op2 = Optional.of(null);
-
+		//nullの場合は空のOptional型を返す
 		Optional<String> op2 = Optional.ofNullable(null);
+		
+		//ofはnull出ない値専用
+		//Optional<String> op2 = Optional.of(null);
+		
+		//nullが格納されてるか見よう（trueなら入ってるfalseなら入ってない）
+		System.out.println(op1.isPresent());
+		System.out.println(op2.isPresent());
+		System.out.println(op1);
 
 	}
 
