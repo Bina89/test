e69078ebbc1b7f30a093da65d9cf17737f7afdcf
H4\PC_H4 <souta.8949.catstar@gmail.com>
2023-05-23 10:15:10 +0900

    int型が引数の関数オブジェクト実装

--- src/lambda/main.java
+++ src/lambda/main.java
@@ -1,6 +1,9 @@
 package lambda;
 
+import java.util.Date;
 import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+import java.util.function.Supplier;
 
 public class main {
 
@@ -13,6 +16,21 @@ public class main {
 
 		System.out.println("文字数 :" + num);
 
+		//引数ないver(日付出力)
+		Supplier<Date> sup = () -> {return new java.util.Date();};
+		System.out.println(sup.get());
+
+		//メソッドaddを格納する関数オブジェクトの作成（int型を使用することが可能なFunctionを使う）
+		IntBinaryOperator bo = main::add;
+		;
+		System.out.println(bo.applyAsInt(10, 90));
+	}
+
+	//引数int2つ
+	//引数の合計を出すメソッドaddを作成
+	//staticを付けるとインスタンス生成しなくても使える
+	public static int add(int a,int b) {
+		return a+b;
 	}
 
 }
